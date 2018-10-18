df96f4f6526dc93991598326c05b1339250b9afe
Steven Huang <steven.charles.huang@gmail.com>
2018-10-17 23:04:49 -0400

    refactored cockpit component to access local references in template

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,8 +8,10 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();;
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  // newServerContent = '';
+  // Pass in the local reference
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
@@ -18,16 +20,17 @@ export class CockpitComponent implements OnInit {
 
   onAddServer(nameInput: HTMLInputElement) {
     // console.log(nameInput.value);
+    console.log(this.serverContentInput);
     this.serverCreated.emit({ 
       serverName: nameInput.value, 
-      serverContent: this.newServerContent 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({ 
       serverName: nameInput.value, 
-      serverContent: this.newServerContent 
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
